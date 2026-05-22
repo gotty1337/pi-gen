71722f52c70ef7d492de4e2e8b788ebf9c697fa8
Thomas Oresnik <thomas.oresnik@anton-paar.com>
2026-05-22 17:12:10 +0200

    change run script of stage0

--- stage0/00-configure-apt/00-run.sh
+++ stage0/00-configure-apt/00-run.sh
@@ -21,7 +21,7 @@ else
 fi
 
 install -m 644 files/raspberrypi-archive-keyring.pgp "${ROOTFS_DIR}/usr/share/keyrings/"
-install -m 644 /usr/share/keyrings/debian-archive-keyring.pgp "${ROOTFS_DIR}/usr/share/keyrings/"
+install -m 644 /usr/share/keyrings/debian-archive-keyring.gpg "${ROOTFS_DIR}/usr/share/keyrings/debian-archive-keyring.pgp"
 on_chroot <<- \EOF
 	ARCH="$(dpkg --print-architecture)"
 	if [ "$ARCH" = "armhf" ]; then
